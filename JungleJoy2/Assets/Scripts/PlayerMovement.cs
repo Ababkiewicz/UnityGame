7a71f1da72fd70458fe918b36df4644f8556a934
PatrykChWIT <77112090+PatrykChWIT@users.noreply.github.com>
2021-01-20 19:21:09 +0100

    v0.2
    Dodany "kręcący" atak z dostosowaniem colidera +czyszczenie assetow

--- JungleJoy2/Assets/Scripts/PlayerMovement.cs
+++ JungleJoy2/Assets/Scripts/PlayerMovement.cs
@@ -7,20 +7,17 @@ public class PlayerMovement : MonoBehaviour
     //VARIABLES
     [SerializeField] private float moveSpeed;    
     private Vector3 moveDirection;
+    public string animationName;
 
-    private bool isNotSliding; // is on a slope or not
-    public float slideFriction = 0.1f; // ajusting the friction of the slope
+    [SerializeField] private bool isNotSliding; // is on a slope or not
+    public float slideFriction = 0.5f; // ajusting the friction of the slope
     private Vector3 hitNormal; //orientation of the slope.
 
-    //[SerializeField] private bool isGrounded;
-    //[SerializeField] private float groundCheckDistance;
-    //[SerializeField] private LayerMask groundMask;
+    private Vector3 hitPoint; //orientation of the slope.
+
     [SerializeField] private float gravity;
-
     [SerializeField] private Vector3 _velocity;
-
-    [SerializeField] private float jumpHeight;
-    private float directionY;
+    [SerializeField] private float jumpHeight;   
 
     //REFERENCES
     private CharacterController controller;
@@ -30,6 +27,7 @@ public class PlayerMovement : MonoBehaviour
     void OnControllerColliderHit (ControllerColliderHit hit) 
     {
         hitNormal = hit.normal;
+        hitPoint = hit.point;
     }
 
     private void Start()
@@ -45,9 +43,9 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Move()
-    {
-        float moveZ = 0;
-        
+    {   
+        animationName= anim.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+        float moveZ = 0;        
         if(isNotSliding)
         {
             moveZ = Input.GetAxis("Vertical");
@@ -81,15 +79,32 @@ public class PlayerMovement : MonoBehaviour
          }
 
         }
+         if(animationName=="Hurricane Kick" )
+         {
+            controller.radius = 2.1f;
+            controller.center = new Vector3(0, 2.1f, 0);
+         }
+         else
+         {
+            controller.radius = 0.6f;
+            controller.center = new Vector3(0, 1.69f, 0); 
+         }
+       
+        if(Input.GetKeyDown(KeyCode.R) && animationName!="Hurricane Kick" )
+         {
+             Attack();
+         }
         
 
                   
 
         _velocity.y += gravity * Time.deltaTime;
         if (!isNotSliding)
-        {
-            moveDirection.x += (1f - hitNormal.y) * hitNormal.x * (1f - slideFriction);
-            moveDirection.z += (1f - hitNormal.y) * hitNormal.z * (1f - slideFriction);
+        {   
+            anim.SetBool("isNotSliding",   isNotSliding);
+            _velocity.y += gravity * Time.deltaTime;
+            moveDirection.x += (1f - hitNormal.y) * hitNormal.x * (slideFriction);
+            moveDirection.z += (1f - hitNormal.y) * hitNormal.z * (slideFriction);
 
         }       
         
@@ -100,6 +115,11 @@ public class PlayerMovement : MonoBehaviour
         //controller.Move(_velocity * Time.deltaTime);
         anim.SetFloat("directionY", Mathf.Abs(_velocity.y), 0.1f, Time.deltaTime);
         anim.SetBool("isGrounded",  controller.isGrounded);
+        anim.SetBool("isNotSliding",   isNotSliding);
+  
+        
+
+        
         
     }
     private void Idle()
@@ -113,6 +133,13 @@ public class PlayerMovement : MonoBehaviour
     private void Jump()
     {
         _velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+
+    }
+    private void Attack()
+    {
+        anim.Play("Hurricane Kick", 0, 0.25f);
+        
+        
 
     }
 }
